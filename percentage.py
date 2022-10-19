33da50fcb089f0b3fe8ffa6ae5860f02b88b2f99
ltifi <ltifisedkiinfo@gmail.com>
2022-10-19 16:14:20 +0100

    add runner score and percentage file\'s solutions

---
+++ percentage.py
@@ -0,0 +1,13 @@
+# Finding the percentage
+# if __name__ == '__main__':
+#     n = int(input())
+#     student_marks = {}
+#     for _ in range(n):
+#         name, *line = input().split()
+#         scores = list(map(float, line))
+#         student_marks[name] = scores
+#     query_name = input()
+#     def average(lst):
+#         return sum(lst)/len(lst)
+#     print(format(average(student_marks[query_name]),'.2f'))
+# ------------------------------------------------------------
